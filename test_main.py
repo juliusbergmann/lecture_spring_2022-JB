251a6ee254af57ec9e5d8a6b6691a39b366ac835
stickler-ci <support@stickler-ci.com>
2022-05-15 21:32:15 +0000

    Fixing style errors.

--- test_main.py
+++ test_main.py
@@ -3,9 +3,9 @@ from main import calculate_module_height, calculate_row_gap
 
 def test_calculate_module_height():
     assert (calculate_module_height(1.755, 10) < 0.304755) and (
-            calculate_module_height(1.755, 10) > 0.30475)
+        calculate_module_height(1.755, 10) > 0.30475)
 
 
 def test_calculate_row_gap():
     assert (calculate_row_gap(1, 45) > 0.999999) and (
-            calculate_row_gap(1, 45) < 1.0000001)
+        calculate_row_gap(1, 45) < 1.0000001)
