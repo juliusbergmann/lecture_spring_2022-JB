befc4cd6a94d55c8ba3517e6acf8eac2b395b617
julius <julius.bergmann@ccore.at>
2022-06-10 17:27:07 +0200

    without assert

--- test_main.py
+++ test_main.py
@@ -4,9 +4,9 @@ import math
 
 
 def test_calculate_module_height():
-    # assert np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
-    assert (calculate_module_height(1.755, 10) < 0.304755) and (
-        calculate_module_height(1.755, 10) > 0.30475)
+    np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
+    # assert (calculate_module_height(1.755, 10) < 0.304755) and (
+    #    calculate_module_height(1.755, 10) > 0.30475)
 
 
 def test_calculate_row_gap():
