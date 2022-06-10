86308aec4454a906a8a1b9b45e1b149355fd4352
julius <julius.bergmann@ccore.at>
2022-06-10 17:34:52 +0200

    change test to numpy-tets

--- test_main.py
+++ test_main.py
@@ -1,11 +1,11 @@
 from main import calculate_module_height, calculate_row_gap
+import numpy as np
+import math
 
 
 def test_calculate_module_height():
-    assert (calculate_module_height(1.755, 10) < 0.304755) and (
-        calculate_module_height(1.755, 10) > 0.30475)
+    np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
 
 
 def test_calculate_row_gap():
-    assert (calculate_row_gap(1, 45) > 0.999999) and (
-        calculate_row_gap(1, 45) < 1.0000001)
+    np.testing.assert_equal(calculate_row_gap(1, 45), 1 / math.tan(45 * 2 * math.pi / 360))
