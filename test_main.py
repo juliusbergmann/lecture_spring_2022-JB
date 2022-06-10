b62a5c39d782323bc6ef914f360e74cd53a1ba26
stickler-ci <support@stickler-ci.com>
2022-06-10 15:33:51 +0000

    Fixing style errors.

--- test_main.py
+++ test_main.py
@@ -4,8 +4,10 @@ import math
 
 
 def test_calculate_module_height():
-    np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
+    np.testing.assert_equal(calculate_module_height(
+        1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
 
 
 def test_calculate_row_gap():
-    np.testing.assert_equal(calculate_row_gap(1, 45), 1 / math.tan(45 * 2 * math.pi / 360))
+    np.testing.assert_equal(calculate_row_gap(
+        1, 45), 1 / math.tan(45 * 2 * math.pi / 360))
