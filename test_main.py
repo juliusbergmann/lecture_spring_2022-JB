60abc503e4d3eb0ba6d84afc41536c787113c347
julius <julius.bergmann@ccore.at>
2022-06-10 17:33:40 +0200

    get test runnin

--- test_main.py
+++ test_main.py
@@ -4,12 +4,8 @@ import math
 
 
 def test_calculate_module_height():
-    np.testing.assert_equal(calculate_module_height(
-        1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
-    # assert (calculate_module_height(1.755, 10) < 0.304755) and (
-    #    calculate_module_height(1.755, 10) > 0.30475)
+    np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
 
 
 def test_calculate_row_gap():
-    assert (calculate_row_gap(1, 45) > 0.999999) and (
-        calculate_row_gap(1, 45) < 1.0000001)
+    np.testing.assert_equal(calculate_row_gap(1, 45), 1 / math.tan(45 * 2 * math.pi / 360))
