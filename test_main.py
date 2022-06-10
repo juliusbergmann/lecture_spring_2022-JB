439618109e8b9133e9a4d4b976452240cd0b6d69
stickler-ci <support@stickler-ci.com>
2022-06-10 15:30:39 +0000

    Fixing style errors.

--- test_main.py
+++ test_main.py
@@ -4,7 +4,8 @@ import math
 
 
 def test_calculate_module_height():
-    np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
+    np.testing.assert_equal(calculate_module_height(
+        1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
     # assert (calculate_module_height(1.755, 10) < 0.304755) and (
     #    calculate_module_height(1.755, 10) > 0.30475)
 
