beecf2832439a868b72e56eaa9ab88bfb2834e65
julius <julius.bergmann@ccore.at>
2022-06-10 17:21:17 +0200

    still testing

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ import math
 
 
 def test_calculate_module_height():
-    assert np.testing.assert_equal(1, 2)
+    assert np.testing.assert_equal(calculate_module_height(1.755, 10), 1.755 * math.sin(10 * 2 * math.pi / 360))
     # assert (calculate_module_height(1.755, 10) < 0.304755) and (
     #    calculate_module_height(1.755, 10) > 0.30475)
 
